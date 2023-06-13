d2dc529999d3d107f6b4112d28e7e0818dbd83f8
Abubakar Adeni <abubakaradeni1@gmail.com>
2023-06-13 23:53:32 +0700

    [add link]

--- src/component/Category.jsx
+++ src/component/Category.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import categoryCSS from "../assets/css/category.module.css";
+import { Link } from "react-router-dom";
 
 function Category({ categoryData }) {
   return (
@@ -8,13 +9,15 @@ function Category({ categoryData }) {
         {categoryData.map((data, index) => {
           return (
             <div key={index.toString()} className="col-lg-4 col-xxl-2 col-6">
-              <div
-                className={`${categoryCSS.categoryItem} d-flex justify-content-center align-items-center rounded-3`}
-                style={{ backgroundColor: `${data.categoryColor}` }}
-              >
-                <img src={data.categoryImg} alt="" />
-                <span className="position-absolute">{data.categoryName}</span>
-              </div>
+              <Link to={`/Kategori/${data.categoryName}`}>
+                <div
+                  className={`${categoryCSS.categoryItem} d-flex justify-content-center align-items-center rounded-3`}
+                  style={{ backgroundColor: `${data.categoryColor}` }}
+                >
+                  <img src={data.categoryImg} alt="" />
+                  <span className="position-absolute">{data.categoryName}</span>
+                </div>
+              </Link>
             </div>
           );
         })}
